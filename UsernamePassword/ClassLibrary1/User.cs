bf91735482f87a423b2b84ac490f85195f9a279e
zaminalirustemov <2g17rck@code.edu.az>
2022-11-25 09:38:56 +0400

    homework

---
+++ UsernamePassword/ClassLibrary1/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class User
+    {
+        public User(string username)
+        {
+            this._userName = username;
+        }
+
+        private string _userName;
+        private string _password;
+        public string UserName
+        {
+            get => _userName;
+            set => _userName = value;
+        }
+        public string Password
+        {
+            get => _password;
+            set => _password = value;
+        }
+
+        public bool HasDigit(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]) == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool HasUpper(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsUpper(input[i]) == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool HasLower(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLower(input[i]) == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ UsernamePassword/UsernamePassword/Program.cs
@@ -0,0 +1,66 @@
+﻿using ClassLibrary1;
+using UsernamePassword;
+namespace UsernamePassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+            string password;
+            bool check;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(">>>>>Welcome to console<<<<<");
+            do
+            {
+
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("---Enter your username: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                username = Console.ReadLine();
+                if (username.Length >= 6 && username.Length <= 25)
+                {
+                    check = false;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("***Error: Username must be between 6-25 characters.");
+                    check = true;
+                }
+            } while (check);
+
+            Console.WriteLine("-------------------------------------");
+
+            User user = new User(username);
+            username = user.UserName;
+            password = user.Password;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("---Enter your password: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                password = Console.ReadLine();
+                if (password.Length >= 8 && password.Length <= 25 && user.HasDigit(password) && user.HasUpper(password) && user.HasLower(password))
+                {
+                    check = false;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("***Error: Password must be set between 8-25 characters and \n          at least 1 uppercase, 1 lowercase letter and 1 number must be used.");
+                    check = true;
+                }
+            } while (check);
+            Console.ResetColor();
+
+            Console.WriteLine("=====================================");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"     Your user information:\n      Username: {username}\n      Password: {password}");
+            Console.ResetColor();
+
+            Console.ReadKey();
+        }
+    }
+}
